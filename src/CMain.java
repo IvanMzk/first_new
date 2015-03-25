d3f00016b932f8a87acd61695709990025727eb2
ivan <ivanmzk@gmail.com>
2015-03-25 20:44:57 +0200

    printing program s args

---
+++ src/CMain.java
@@ -0,0 +1,16 @@
+
+public class CMain {
+
+    public static void main(String[] argv)
+    {
+        //printing values of argv array and number of array elements
+        if (argv.length > 0) {
+            for (String arg : argv)
+            {
+                System.out.println(arg);
+            }
+            System.out.printf("%d parameters passed", argv.length);
+        }
+        else{System.out.println("No arguments were passed");}
+    }
+}
